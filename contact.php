17cf35a7e527207cb1e38dc2164ef8f86783ab62
asifbhuiyann <getconnectwithasif@gmail.com>
2023-11-29 18:54:07 +0600

    Latest commit

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <!-- adding google font cdn here  -->
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&family=Montserrat&family=Satisfy&display=swap"
+        rel="stylesheet">
+
+
+    <!-- adding font awesome cdn here  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="contact.css">
+</head>
+
+<body>
+    <div class="containerr">
+        <div class="contact-links col-4" id="contact">
+            <h1>Ways to Contact</h1>
+            <hr class="dotted-hr">
+
+            <div class="media-buttons">
+
+                <button class="btn" onclick="window.open('https://www.facebook.com/aasifbhuiyan/')"
+                    class="btn request-callback"><i class="fa fa-facebook" style="color:  #3b5998;"
+                        aria-hidden="true"></i></button>
+                <button class="btn" onclick="window.open('https://about.me/asif.bhuiyan')"
+                    class="btn request-callback"> <span style="font-size: 1.7rem;">&#127760;</span> </i></button>
+                <button class="btn" onclick="window.open(' https://github.com/asifbhuiyann')"
+                    class="btn request-callback"><i class="fa fa-github" aria-hidden="true"
+                        style="color: #000000;"></i></button>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
